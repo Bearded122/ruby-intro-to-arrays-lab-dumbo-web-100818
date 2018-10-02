657c6366da51c0cd447fd1191b294ba81eecf59b
John Sime <johnc.sime@gmail.com>
2018-10-02 06:48:35 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,8 +18,8 @@ def last_element(my_last_element)
   my_last_element[-1]
 end
 
-def first_country(first_country)
-  first_country.first
+def first_country(first_element_with_array_methods)
+  first_element_with_array_methods.first
 end
 
 def last_element(my_last_country)
